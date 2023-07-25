15114b0283bad893d4d6d781375d8b7473a4e9b2
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-07-25 16:25:32 -0300

    refactored

---
+++ src/components/Entry.js
@@ -0,0 +1,22 @@
+import emojipedia from "../emojipedia"
+
+
+function Entry(props) {
+    return (
+        <dl className="dictionary">
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              {props.emoji}
+            </span>
+            <span>{props.name}</span>
+          </dt>
+          <dd>
+           {props.meaning}
+          </dd>
+        </div>
+      </dl>
+    )
+}
+
+export default Entry;
